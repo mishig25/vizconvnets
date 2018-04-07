8cf713822b5b0ac9c9192012ff755df0f29df1c5
Mishig Davaadorj <dmishig@gmail.com>
2018-04-07 00:04:35 -0600

    feat: grabage collection

--- frontend/engine.js
+++ frontend/engine.js
@@ -1,17 +1,19 @@
 export default class Engine{
   constructor(){
     this.sprites = [];
+    this.direction = 'left';
     this.game = new Phaser.Game(600, 600, Phaser.CANVAS, 'renderArea', { create: this.create, update: this.update.bind(this) });
   }
   create(){
-    this.game.stage.backgroundColor = 0xbada55;
+    // this.game.stage.backgroundColor = 0xbada55;
+    this.game.stage.backgroundColor = 0xffffff;
   }
   update(){
     if(this.sprites.length > 0){
       for(var i=0;i<this.sprites.length;i++){
         const item = this.sprites[i];
-        var {sprite,degree,radius,translate} = item;
-        degree = degree + .2;
+        var {sprite,degree,radius,translate,direction} = item;
+        degree = degree + .2*direction;
         const {x,y} = this.calculateCoordinates(degree,radius,translate);
         sprite.x = x;
         sprite.y = y;
@@ -19,7 +21,15 @@ export default class Engine{
       }
     }
   }
+  clear(){
+    for(var i=0;i<this.sprites.length;i++){
+      const { sprite } = this.sprites[i];
+      sprite.kill();
+    }
+    this.sprites = [];
+  }
   renderChannels(layers){
+    this.clear();
     this.plotActivationLayer(layers[0],.5,100);
     this.plotActivationLayer(layers[1],3,175);
     this.plotActivationLayer(layers[2],2.5,240);
@@ -32,7 +42,11 @@ export default class Engine{
     const y = Math.sin(this.toRadians(degree))*radius+translate;
     return { x, y };
   }
+  updateRotateDirection(){
+    this.direction = this.direction == 'left' ? 'right' : 'left';
+  }
   plotActivationLayer(data, scale, radius){
+    this.updateRotateDirection();
     const offset = 360/data.length;
     for(var i=0; i<data.length; i++){
       const channelData = data[i];
@@ -40,11 +54,12 @@ export default class Engine{
       var bmd = this.game.add.bitmapData(width,width);
       bmd.ctx.putImageData(channelData,0,0);
       const degree = i*offset;
-      const translate = 300-(scale*width/2)
+      const translate = 300-(scale*width/2);
+      const direction = this.direction == 'left' ? -1 : 1;
       const {x,y} = this.calculateCoordinates(degree,radius,translate);
       var sprite = this.game.add.sprite(x, y, bmd);
       sprite.scale.setTo(scale,scale);
-      this.sprites.push({ sprite,degree,radius,translate });
+      this.sprites.push({ sprite,degree,radius,translate,direction });
     }
   }
 }
