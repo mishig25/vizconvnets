036cfa87fc3a9b952a095450d02dc753354936cd
Mishig Davaadorj <dmishig@gmail.com>
2018-04-06 20:43:23 -0600

    feat: sample images

--- frontend/index.js
+++ frontend/index.js
@@ -1,10 +1,21 @@
 import Model from './model'
 import Engine from './engine'
+import {IMAGE_URLS} from './sampleImages'
 
 var canvas = document.getElementById("helperCanvas");
 var ctx = canvas.getContext("2d");
-document.getElementById('file').addEventListener('change', handleFileSelect, false);
+document.getElementById('file').addEventListener('change', fileUploaded, false);
+document.getElementById('sampleImages').addEventListener('change', sampleImageChosen, false);
+
+IMAGE_URLS.forEach((sample) => {
+  var option = document.createElement('option')
+  option.setAttribute("value", sample.value);
+  option.innerText = sample.text;
+  document.getElementById('sampleImages').appendChild(option);
+})
+
 const imgElement = document.getElementById('img');
+imgElement.setAttribute('crossorigin', 'anonymous');
 
 let model, engine;
 
@@ -15,25 +26,29 @@ async function init() {
   model.warmUp();
 }
 
-function handleFileSelect(evt) {
+function fileUploaded(evt) {
   var reader = new FileReader();
-  reader.onload = e => {
-    imgElement.src= e.target.result;
-    imgElement.onload = () => predict(imgElement);
-  }
+  reader.onload = e => predict(e.target.result,imgElement);
   // Read in the image file as a data URL.
   reader.readAsDataURL(evt.target.files[0]);
 }
 
-async function predict(imgElement){
-  if(!model.loaded) await model.loadModel();
+function sampleImageChosen(e) {
+    predict(e.target.value,imgElement);
+}
 
-  const preds = await model.predict(imgElement);
+async function predict(imgPath,imgElement){
+  imgElement.src= imgPath;
+  imgElement.onload = async () => {
+    if(!model.loaded) await model.loadModel();
 
-  const layer1 = await model.getActivation(canvas,0,8);
-  const layer2 = await model.getActivation(canvas,1,16);
-  const layer3 = await model.getActivation(canvas,2,32);
-  engine.renderChannels([layer1,layer2,layer3]);
+    const preds = await model.predict(imgElement);
+
+    const layer1 = await model.getActivation(canvas,0,8);
+    const layer2 = await model.getActivation(canvas,1,16);
+    const layer3 = await model.getActivation(canvas,2,32);
+    engine.renderChannels([layer1,layer2,layer3]);
+  };
 }
 
 init();
